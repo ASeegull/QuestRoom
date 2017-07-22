f70482f175501c20dea9f4135ce01d653e103c13
Anna <a.chaikalemberg@gmail.com>
2017-07-22 13:47:09 +0300

    Adds template of main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-/* requirements */
-
-var gulp = require('gulp'),
+let gulp = require('gulp'),
   sass = require('gulp-sass'),
   browserSync = require('browser-sync').create(),
   runSequence = require('run-sequence'),
@@ -10,38 +8,19 @@ var gulp = require('gulp'),
   destination = 'docs',
   cssnano = require('gulp-cssnano'),
   prefixer = require('gulp-autoprefixer'),
-  // babel = require('gulp-babel'),
   uglify = require('gulp-uglify');
 
-//
-// -----------------------------------------------------------------------------
-// dev-tasks
 
-
-// compils sass to css and adds vendor prefixes 
 gulp.task('sass', () => {
   return gulp.src('dev/styles/*.sass')
     .pipe(sass())
     .pipe(prefixer({
-      browsers: ['last 3 versions'],
+      browsers: ["last 3 version", "> 1%", "ie 8", "ie 9", "Opera 12.1"],
       cascade: false
     }))
     .pipe(gulp.dest('dev/styles'))
 });
 
-
-// // looks for errors and warnings and compiles .js to es5
-// gulp.task('js', () => {
-//   return gulp.src('dev/es6/script.js')
-//     .pipe(jshint('.jshintrc'))
-//     .pipe(jshint.reporter('default'))
-//     .pipe(babel({
-//       presets: ['es2015']
-//     }))
-//     .pipe(gulp.dest('dev/scripts'));
-// });
-
-// reloads browser-sync on change of html, css, js files 
 gulp.task('browse', ['sass'], () => {
   browserSync.init({
     server: {
@@ -55,19 +34,14 @@ gulp.task('browse', ['sass'], () => {
   browserSync.watch(['dev/*.html', 'dev/styles/*.css', 'scripts/*.js']).on('change', browserSync.reload);
 });
 
-//
-// -----------------------------------------------------------------------------
-// prepares files for release
 
-// cleans dir for release
 gulp.task('del', () => {
   return del(destination)
 });
 
-// simply copies files, that don't need additional processing
 
 gulp.task('copy', () => {
-  var sourceFiles = ['dev/images/*.png', 'dev/fonts/*.*', 'dev/*.html'];
+  let sourceFiles = ['dev/images/*.png', 'dev/fonts/*.*', 'dev/*.html'];
   return gulp
     .src(sourceFiles)
     .pipe(copy(destination, {
@@ -75,25 +49,20 @@ gulp.task('copy', () => {
     }));
 });
 
-// gathers all css to one file and minifies them 
 gulp.task('css-prod', function() {
   return gulp.src('dev/styles/*.css')
     .pipe(concat('style.css'))
     .pipe(cssnano())
     .pipe(gulp.dest('docs/styles'));
-})
+});
 
-// gathers all js files (already babelified) to one file and minifies them 
 gulp.task('js-prod', () => {
   return gulp.src(['dev/scripts/*.js'])
     .pipe(uglify())
     .pipe(gulp.dest('docs/scripts'));
-})
-
+});
 
 gulp.task('default', ['browse']);
-//
-//gulp.task('prod', ['del', 'copy', 'css-prod', 'js-prod']);
 
 gulp.task('prod', function() {
   runSequence('del', ['css-prod'], 'copy');
